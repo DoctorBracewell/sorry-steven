6f622a9d2caf59188599ec22fd04e021898da28e
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-02-07 22:01:46 +0000

    Added scene nav and sceneeffects

--- interface/src/scenes/IntroScene.ts
+++ interface/src/scenes/IntroScene.ts
@@ -18,7 +18,13 @@ export class IntroScene implements Scene {
     this.p.text('Welcome to the Sad Game. Press any key to continue.', this.p.width / 2, this.p.height / 2);
   }
 
-  draw(): void {}
+  draw(): void {
+    this.p.background(50);
+    this.p.fill(255);
+    this.p.textAlign(this.p.CENTER, this.p.CENTER);
+    this.p.textSize(32);
+    this.p.text('Welcome to the Sad Game. Press any key to continue.', this.p.width / 2, this.p.height / 2);
+  }
 
   keyPressed(): void {
     console.log('Switching to Game Scene.');
