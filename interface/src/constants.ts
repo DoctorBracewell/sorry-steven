b80a5ee585eb10694c2cfd9123f6602e23de237a
Brace <11965313+DoctorBracewell@users.noreply.github.com>
2025-02-07 20:41:50 +0000

    file: structure

--- interface/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const BACKGROUND_COLOUR = 0x292929;
-// export const WIDTH = 1280;
-// export const HEIGHT = 720;
-export const WIDTH = 1120;
-export const HEIGHT = 630;
-export const TILESIZE = 30;
--- interface/src/entities/BackgroundObject.ts
+++
@@ -1,9 +0,0 @@
-import { Sprite, Texture } from "pixi.js";
-import Entity from "./Entity";
-import { Scene } from "../engine/Scene";
-
-export default class BackgroundObject extends Entity {
-	constructor(width: number, height: number, texture: Texture, scene: Scene) {
-		super(width, height, texture, scene);
-	}
-}
