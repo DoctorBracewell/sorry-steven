a5f19c9968b63fcc3a0e036d49a4e2b481deb44d
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-02-08 14:59:20 +0000

    Adding time to queue queue

--- interface/src/game/manager.ts
+++ interface/src/game/manager.ts
@@ -1,18 +1,49 @@
 import p5 from "p5";
 import { GameState } from "./gameState";
+import { QueueHandler, THEENUM } from "./queueHandler";
 
 export class Manager {
-  secondsPast: number = 0;
-  p: p5;
 
-  constructor(p: p5) {
-    this.p = p;
-  }
+    secondsPast: number = 0;
+    p: p5;
+    next_choice: number;
 
-  update(): void {
-    this.secondsPast += this.p.deltaTime / 1000;
-    GameState.setBPM(this.secondsPast);
-  }
+    qh: QueueHandler;
+
+    constructor(p: p5) {
+        this.p = p;
+        this.next_choice = this.get_next_choice_time();
+        this.qh = new QueueHandler();
+    }
+
+    update(): void {
+        this.secondsPast += this.p.deltaTime / 1000;
+        GameState.setBPM(this.secondsPast);
+
+        if (this.next_choice < this.secondsPast) {
+
+            const choiceType = this.qh.sendNewChoice();
+
+            let time_offset: number = 0;
+            if (choiceType == THEENUM.Colours) {
+                time_offset = 0; // To change
+            } else if (choiceType == THEENUM.Vibrations) {
+                time_offset = 240 / GameState.bpm;
+            } else {
+                time_offset = 0; // To Change
+            }
+             
+
+            this.next_choice = time_offset + this.get_next_choice_time();
+        }
+
+    }
+
+    get_next_choice_time(): number {
+
+        return this.secondsPast + Math.max(3 - GameState.bpm / 80, 0) + Math.random();
+
+    }
 
 
 }
