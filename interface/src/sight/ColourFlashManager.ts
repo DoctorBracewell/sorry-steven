edacd827c76c7c8acdcbbb3bd988665061aaa5fc
jessicapeck <jess@pecksplace.com>
2025-02-08 20:06:03 +0000

    Remove log messages and test function

--- interface/src/sight/ColourFlashManager.ts
+++ interface/src/sight/ColourFlashManager.ts
@@ -14,12 +14,10 @@ export class ColourFlashManager {
     private p:p5;
 
     constructor(p: p5) {
-        console.log("constructor");
         this.p = p;
     }
 
     public setGradients(colourSequence: Colours[], bpm: number) {
-        console.log("colourSequence");
         const duration = (60 / bpm) * 1000;
 
         let index = 0;
@@ -27,7 +25,6 @@ export class ColourFlashManager {
         const flashNextColour = () => {
             for (let y = 0; y < CANVAS_HEIGHT; y++) {
                 let alpha = this.p.map(y, 0, CANVAS_HEIGHT, 50, 200);
-                console.log(colourSequence[index]);
                 let [r, g, b] = ColourRGBs[colourSequence[index]];
 
                 this.p.stroke(r, g, b, alpha);
@@ -45,13 +42,5 @@ export class ColourFlashManager {
 
         flashNextColour();
         
-    }
-
-    public test() {
-        console.log("test");
-        this.p.stroke(255, 0, 0); // Force red for visibility
-        this.p.strokeWeight(100)
-        this.p.line(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // Draw a simple diagonal line
-        this.p.circle(0, 0, 100)
     }
 }
