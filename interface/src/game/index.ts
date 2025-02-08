1d13f85373606daeb0de4efd8557a6b7eeeed5d4
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-02-08 00:39:33 +0000

    VIBRATING FOR THE GODS :kiss:

--- interface/src/game/index.ts
+++ interface/src/game/index.ts
@@ -1,3 +1,5 @@
+import { SERVER_URL } from "../constants";
+
 
 enum Coulors {
     Red = "RED",
@@ -28,13 +30,66 @@ export class Game {
 
     private static previousChoice = null;
 
+    private static onBeatCount = 4;
+    private static beatCount = 8;
+
     private static sendNewChoice(): void {
 
-        const enumSet: Set<any> = new Set([Colors, Sizes, Shapes]);
+        const enumSet: Set<any> = new Set([Coulors, Sound, Vibration]);
+        if (this.previousChoice !== null) {
+            enumSet.delete(this.previousChoice);
+        }
+
+        const enumsArray = Array.from(enumSet);
+        const choice = enumsArray[Math.floor(Math.random() * enumsArray.length)];
+
+        if (choice == Vibration) {
+
+            this.addNewVibration();
+
+        } else if (choice == Sound) {
+
+        } else if (choice == Coulors) {
+
+        }
 
     }
-    
 
+    private static addNewSounds() {}
+
+    private static addNewVibration() {
+
+        let beats = Array(this.beatCount).fill(0);
+
+        const shuffled = [0, 1, 2, 3, 4, 5, 6, 7].sort(() => 0.5 - Math.random());
+        const on_beats = shuffled.slice(0, this.beatCount);
+        on_beats.forEach(on_beat => beats[on_beat] = 1)
+
+        const payload = {
+            sequence: JSON.stringify(on_beats),
+            bpm: this.bpm.toString(),
+        };
+
+        fetch(SERVER_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json" // Ensure the server knows you're sending JSON
+            },
+            body: JSON.stringify(payload)
+        })
+
+    }
+
+    private static addNewCoulors() {
+
+        const values = Object.values(Coulors);
+
+        // for (let i = 0; i < n; i++) {
+        //     const coulor = values[Math.floor(Math.random() * values.length)];
+        //     this.queue.push(coulor);
+        // }
+
+    }
 
 
 }
