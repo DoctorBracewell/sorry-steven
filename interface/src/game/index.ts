4353db4a72f698a2a1d467f0519467d3cf696810
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-02-08 10:27:14 +0000

    user can do da mash

--- interface/src/game/index.ts
+++ interface/src/game/index.ts
@@ -11,7 +11,8 @@ enum Coulors {
 
 enum THEENUM {
     Sound,
-    Coulors
+    Coulors,
+    Vibrations
 }
 
 function shuffleArray<T>(array: T[]): T[] {
@@ -26,12 +27,13 @@ export class Game {
 
     private static bpm: number = 160;
     
-    private static queue = [];
+    private static queue: [THEENUM, any[]][] = [];
 
     private static previousChoice: string = "";
 
     private static onBeatCount = 4;
     private static beatCount = 8;
+    private static userBeats: number[] = [];
 
     private static soundTypeCount = 2;
     private static soundCounts = 4
@@ -74,6 +76,10 @@ export class Game {
         let media_player = new Sound();
         media_player.playSound(sounds, this.bpm);
 
+        this.queue.push(
+            [THEENUM.Sound, sounds]
+        );
+
     }
 
     public static addNewVibration() {
@@ -96,7 +102,11 @@ export class Game {
               "Content-Type": "application/json"
             },
             body: JSON.stringify(payload)
-        })
+        });
+
+        this.queue.push(
+            [THEENUM.Vibrations, beats]
+        );
 
     }
 
@@ -113,8 +123,64 @@ export class Game {
 
         // Send coulors
 
+        this.queue.push(
+            [THEENUM.Coulors, coulors]
+        );
+
     }
 
-    public static sendInput(input: string) {}
+    public static sendInput(input: string): boolean {
+
+        const [curr_type, curr_seq] = this.queue[0];
+
+        switch (curr_type) {
+            case THEENUM.Coulors:
+                if (
+                    input == "q" && curr_seq[0] == Coulors.Red
+                    || input == "w" && curr_seq[0] == Coulors.Yellow
+                    || input == "e" && curr_seq[0] == Coulors.Green
+                    || input == "r" && curr_seq[0] == Coulors.Pink
+                ) {
+                    this.popQueue();
+                    return true;
+                }
+                break;
+            case THEENUM.Sound:
+                if (
+                    input == "a" && curr_seq[0] == 0
+                    || input == "s" && curr_seq[0] == 1
+                    || input == "d" && curr_seq[0] == 2
+                    || input == "f" && curr_seq[0] == 3
+                ) {
+                    this.popQueue();
+                    return true;
+                }
+                break;
+            case THEENUM.Vibrations:
+                if (input == " ") {
+                    this.userBeats.push(Date.now() / 1000);
+                    const beatsNeeded = this.queue[0][1].reduce((acc, curr) => acc + curr, 0);
+                    if (this.userBeats.length == beatsNeeded) {
+                        // Check if correct
+
+                        this.userBeats = [];
+                    } // Can't lose multiple lives in less than a second (i-frames)
+                    return true;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return false;
+
+    }
+
+    private static popQueue() {
+        this.queue[0][1].shift();
+        if (this.queue[0][1].length == 0) {
+            this.queue.shift();
+        }
+    }
 
 }
