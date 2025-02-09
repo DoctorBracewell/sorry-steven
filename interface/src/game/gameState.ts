332d16c7e2f7534a0db7bfdf161ef87794abb5e5
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-02-09 16:11:32 +0000

    Move sound

--- interface/src/game/gameState.ts
+++ interface/src/game/gameState.ts
@@ -16,7 +16,7 @@ export class GameState {
     public static onBeatCount: number = 4;
     public static beatCount: number = 8;
     public static soundTypeCount: number = 2;
-    public static soundCounts: number = 2;
+    public static soundCounts: number = 3;
     public static coloursCount: number = 2;
 
     public static pause: boolean = false;
